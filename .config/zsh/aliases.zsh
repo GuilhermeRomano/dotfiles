b1fb5540df8545ced816b5a9b3e5cff9dac21b31
Guilherme Romano <guilherme.vinicius121@gmail.com>
2024-03-25 10:46:45 -0300

    Add zsh aliases file

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,46 @@
+# alias for oh-my-zsh
+
+# Disable alias made by oh-my-zsh
+unalias lsa
+unalias l
+unalias la
+
+# ls alias
+alias ls='exa --group-directories-first --icons --all --across'
+alias ll='exa --group-directories-first --icons --all --long --header'
+alias tree='exa --group-directories-first --icons --all --tree --level=2'
+
+# other utilities alias
+alias vim='nvim'
+alias trash='gio trash'
+alias bat='batcat'
+
+# zsh alias 
+alias zshconfig="nvim ~/.config/zsh/.zshrc"
+
+# cd alias
+alias dotfiles="cd ~/dotfiles"
+
+# fzf alias
+alias fman="compgen -c | fzf | xargs man"
+
+# protontricks alias
+alias protontricks='flatpak run com.github.Matoking.protontricks'
+alias protontricks-launch='flatpak run --command=protontricks-launch com.github.Matoking.protontricks'
+
+# w3m
+alias w3m='w3m -config ~/.config/w3m/config'
+
+# set apt to call nala instead
+apt() { 
+  command nala "$@"
+}
+sudo() {
+  if [ "$1" = "apt" ]; then
+    shift
+    command sudo nala "$@"
+  else
+    command sudo "$@"
+  fi
+}
+
