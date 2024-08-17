d197cd1b2ec43db82a5318e65d627c171eb2c708
Guilherme Romano <guilherme.vinicius121@gmail.com>
2024-08-17 14:21:14 -0300

    Revert "Change nvim theme to nord"
    This reverts commit e30121c7df77eb65553972a12490f380ac4b20d6.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -741,16 +741,18 @@ require("lazy").setup({
 	--},
 
 	{ -- Current Theme
-		"gbprod/nord.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		lazy = false,
 		priority = 1000,
+
 		config = function()
-			require("nord").setup({})
-			vim.cmd.colorscheme("nord")
+			require("catppuccin").setup({
+				flavor = "mocha",
+			})
+			-- setup must be called before loading
+			vim.cmd.colorscheme("catppuccin")
 		end,
-	},
-	install = {
-		colorscheme = { "nord" },
 	},
 
 	-- Highlight todo, notes, etc in comments
